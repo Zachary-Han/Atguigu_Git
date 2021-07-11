c131c6cce2e1428dfa9c0d4340a292b2baa649e9
zachary <248771493@qq.com>
2021-07-11 18:07:08 +0800

    dev first

---
+++ src/main/java/com/zachary2.java
@@ -0,0 +1,13 @@
+package com;
+
+/**
+ * @author : Zachary
+ * @ClassName : zachary2
+ * @Description :
+ * @Date: 2021-07-11 18:06
+ */
+public class zachary2 {
+    public static void main(String[] args) {
+        System.out.println("dev");
+    }
+}
